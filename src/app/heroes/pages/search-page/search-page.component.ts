8696ed5e86e38f06257dc8843899d70648bef875
Giselle <ABASLEALGIS@GMAIL.COM>
2024-04-16 16:23:12 -0600

    fin sección 14_2

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -12,6 +13,7 @@ export class SearchPageComponent {
   //reactive form module, usar cada que necesitemos implementar un formulario
   public searchInput = new FormControl('');
   public heroes: Hero[] = [];
+  public selectedHero?: Hero;
 
   //Para usar un servicio hay que inyectarlo en el constructor
   constructor(private heroesService:HeroesService) { }
@@ -22,6 +24,18 @@ export class SearchPageComponent {
       //la variable de heroes se iguala a la respuesta del servicio
       .subscribe(heroes => this.heroes = heroes);
 
+    }
+
+    onSelectedOption(event: MatAutocompleteSelectedEvent):void{
+      if (!event.option.value){
+        this.selectedHero = undefined
+        return;
+      }
+
+      const hero: Hero = event.option.value;
+      this.searchInput.setValue(hero.superhero);
+
+      this.selectedHero=hero;
     }
 
 }
